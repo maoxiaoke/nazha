0a3ae1fd21e36c0f762368e1a1a8a88c4d12d124
maoxiaoke <maoxiaoke@outlook.com>
2023-10-17 17:32:22 +0800

    feat: almost done

--- pages/hackernews-top-archive.tsx
+++ pages/hackernews-top-archive.tsx
@@ -1,31 +1,32 @@
-'use client';
+import { format } from 'light-date';
+import { useRef, useState } from 'react';
+import { CalendarDays } from 'lucide-react';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
-import { format, localeFormat } from 'light-date';
-import { useEffect, useRef } from 'react';
-// import Datepicker from 'flowbite-datepicker/Datepicker';
 import Image from 'next/image';
 import Link from 'next/link';
 import Head from 'next/head';
-import dynamic from 'next/dynamic';
-
-// const Datepicker = dynamic(() => import('flowbite-datepicker/Datepicker'), {
-//   ssr: false
-// });
+import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const date = new Date();
-
-// const Datepicker = require('flowbite-datepicker/Datepicker');
+let datePickerInstance = null;
 
 const HackNewsTopArchive = () => {
   const datepickerEl = useRef<HTMLDivElement>(null);
+  const [viewType, setViewType] = useState<'day' | 'month' | 'year' | string>('month');
+  const [datepickerShowed, setDatepickerShowed] = useState(false);
 
-  useEffect(() => {
-    if (datepickerEl.current) {
-      // @ts-ignore
-      import('flowbite-datepicker/Datepicker').then(({ default: Datepicker }) => {
-        const a = new Datepicker(datepickerEl.current, {
-          // options
-          pickLevel: 2,
+  const selectDate = (evt) => {
+    evt.stopPropagation();
+    if (datePickerInstance) {
+      return;
+    }
+
+    import('flowbite-datepicker/Datepicker').then(({ default: Datepicker }) => {
+      if (datepickerEl.current) {
+        const pickLevel = viewType === 'day' ? 0 : viewType === 'month' ? 1 : 2;
+        datePickerInstance = new Datepicker(datepickerEl.current, {
+          pickLevel: pickLevel,
           minDate: new Date('2006-01-01'),
           maxDate: Date.now()
         });
@@ -35,45 +36,84 @@ const HackNewsTopArchive = () => {
             const value = e.detail.date;
             console.log('value', format(value, '{yyyy}'));
           });
+
+          datepickerEl.current.addEventListener('focus', () => {
+            console.log('blue---');
+          });
         }
 
-        console.log('fsafd', a);
-      });
-    }
-  }, []);
+        console.log('fsafd', datePickerInstance);
+      }
+    });
+  };
+
   return (
     <>
       <Head>
         <link href="/styles/flowbite.css" rel="stylesheet" />
       </Head>
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-          <Link href="/hackernews-top-archive" className="flex items-center">
-            <Image
-              className="w-8 h-8 overflow-hidden mr-3"
-              src="/images/hn.png"
-              alt="Hacker News Cute Logo"
-              width="32"
-              height="32"
-            />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Top Archive
-            </span>
-          </Link>
-          <div className="flex items-center">
-            <a href="#" className="text-sm text-blue-600 dark:text-blue-500 hover:underline">
-              Login
-            </a>
+      <div
+        className="h-screen"
+        onClick={() => {
+          console.log('oter click');
+          if (datePickerInstance) {
+            // @ts-ignore
+            datePickerInstance.destroy();
+            datePickerInstance = null;
+          }
+        }}>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+            <Link href="/hackernews-top-archive" className="flex items-center">
+              <Image
+                className="w-8 h-8 overflow-hidden mr-3"
+                src="/images/hn.png"
+                alt="Hacker News Cute Logo"
+                width="32"
+                height="32"
+              />
+              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                Top Archive
+              </span>
+            </Link>
+            <div className="flex items-center">
+              <a href="#" className="text-sm text-blue-600 dark:text-blue-500 hover:underline">
+                Login
+              </a>
+            </div>
           </div>
+        </nav>
+
+        <div className="max-w-screen-sm mx-auto">
+          <header className="flex flex-wrap justify-between border-solid border-b py-4">
+            <div className="text-3xl font-catamaran text-gray-400">{format(date, '{yyyy}')}</div>
+
+            <div className="flex items-center">
+              <Tabs defaultValue={viewType} onValueChange={(type) => setViewType(type)}>
+                <TabsList>
+                  <TabsTrigger value="day">Day</TabsTrigger>
+                  <TabsTrigger value="month">Month</TabsTrigger>
+                  <TabsTrigger value="year">Year</TabsTrigger>
+                </TabsList>
+              </Tabs>
+
+              <div className="relative">
+                <CalendarDays
+                  className="inline-block ml-4 cursor-pointer"
+                  color="rgb(255,102,0)"
+                  onClick={selectDate}
+                />
+                <div
+                  ref={datepickerEl}
+                  data-date="02/25/2022"
+                  className="absolute top-8 left-0"
+                  onClick={(evt) => {
+                    evt.stopPropagation();
+                  }}></div>
+              </div>
+            </div>
+          </header>
         </div>
-      </nav>
-
-      <div className="max-w-screen-sm mx-auto">
-        <header className="flex flex-wrap justify-between border-solid border-b py-10">
-          <div className="text-3xl font-catamaran text-gray-400">{format(date, '{yyyy}')}</div>
-
-          <div ref={datepickerEl} data-date="02/25/2022"></div>
-        </header>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,13 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         gothamsm: ['var(--font-gothamsm)'],
@@ -33,6 +40,7 @@ module.exports = {
 
       colors: {
         ...colors,
+        hacker: 'rgb(255,102,0)',
         purple: '#3f3cbb',
         unhovered: '#d1e8f3ed',
         'unhovered-dark': '#2b4555',
@@ -50,17 +58,60 @@ module.exports = {
         muted: 'var(--color-muted)',
         accent: 'var(--color-link-posts)',
         subscrible: '#FF354D',
-        subscribleRight: '#FE6108'
+        subscribleRight: '#FE6108',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
       },
       animation: {
-        gradient: 'gradient 10s ease infinite'
+        gradient: 'gradient 10s ease infinite',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       keyframes: {
         gradient: {
           '0%': { 'background-position': '0% 100%' },
           '50%': { 'background-position': '100% 50%' },
           '100%': { 'background-position': '0% 100%' }
-        }
+        },
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
       },
       backgroundImage: {
         iridescent: 'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)'
@@ -74,9 +125,9 @@ module.exports = {
   variants: {
     extend: {}
   },
-  plugins: [hoverPlugin, require('flowbite/plugin')],
+  plugins: [hoverPlugin, require('flowbite/plugin'), require("tailwindcss-animate")],
 
   content: [
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js", "app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"
   ]
 };
