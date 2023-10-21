b7865d41af93f6923e5cf09d0545bc3dbc08ebeb
maoxiaoke <maoxiaoke@outlook.com>
2023-10-21 11:22:33 +0800

    feat: corlor

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -27,7 +27,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center font-medium whitespace-nowrap rounded px-5 py-1.5 text-sm ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-forground data-[state=active]:shadow-sm',
+      'inline-flex items-center justify-center font-medium whitespace-nowrap rounded px-5 py-1.5 text-sm ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-[#fff] data-[state=active]:text-forground data-[state=active]:shadow-sm',
       className
     )}
     {...props}
