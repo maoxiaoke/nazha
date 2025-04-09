f7d676c6821d9de41b94f3f7f9c11a830a292543
maoxiaoke <maoxiaoke@outlook.com>
2025-04-09 17:47:39 +0800

    feat: add Clarity tracking script to document head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,20 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "r1e18db9hh");
+            `
+          }}
+        />
+      </Head>
       <body className="dark">
         <script
           dangerouslySetInnerHTML={{
